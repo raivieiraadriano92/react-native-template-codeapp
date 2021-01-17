dede3aedf17aa8215e09f67c5abd988ed4339caf
Raí Vieira Adriano <raivieiraadriano92@gmail.com>
2021-01-16 21:13:53 -0300

    added ScrollViewFaded component

---
+++ template/src/components/molecules/scrollview-faded/index.tsx
@@ -0,0 +1,58 @@
+import React, { PropsWithChildren } from 'react'
+import { ScrollView, ScrollViewProps } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+import { Flex } from 'src/components/atoms'
+
+type Props = ScrollViewProps &
+  PropsWithChildren<{
+    colors: Partial<Record<'bottom' | 'top', string[]>>
+    disableBottomInset?: boolean
+    disableTopInset?: boolean
+  }>
+
+export default function ({
+  children,
+  colors,
+  disableBottomInset = false,
+  disableTopInset = false,
+  ...props
+}: Props): JSX.Element {
+  const insets = useSafeAreaInsets()
+
+  const heightBottom = 48 + (disableBottomInset ? 0 : insets.bottom)
+  const heightTop = 48 + (disableTopInset ? 0 : insets.top)
+
+  return (
+    <Flex flex>
+      <ScrollView {...props}>
+        <Flex style={{ height: heightTop }} />
+        {children}
+        <Flex style={{ height: heightBottom }} />
+      </ScrollView>
+      {!!colors.top && (
+        <LinearGradient
+          colors={colors.top}
+          style={{
+            height: heightTop,
+            position: 'absolute',
+            top: 0,
+            width: '100%'
+          }}
+        />
+      )}
+      {!!colors.bottom && (
+        <LinearGradient
+          colors={colors.bottom}
+          style={{
+            bottom: 0,
+            height: heightBottom,
+            position: 'absolute',
+            width: '100%'
+          }}
+        />
+      )}
+    </Flex>
+  )
+}
--- template/src/components/pages/home/index.tsx
+++ template/src/components/pages/home/index.tsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import { ScrollView } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
+import { useTheme } from 'styled-components/native'
 
 import { Flex, Icon, Space, Text } from 'src/components/atoms'
-import { Button, Input } from 'src/components/molecules'
+import { Button, Input, ScrollViewFaded } from 'src/components/molecules'
 import { useGeneralContext } from 'src/contexts/general'
 import { RootStackNavigator } from 'src/navigator'
+import { hexToRgba } from 'src/utils'
 
 export default function (): JSX.Element {
+  const generalContext = useGeneralContext()
+
   const navigation = useNavigation<StackNavigationProp<RootStackNavigator>>()
 
   const [text, setText] = useState<string>('Some title')
 
-  const generalContext = useGeneralContext()
+  const theme = useTheme()
 
   useEffect(() => {
     navigation.setOptions({
@@ -32,146 +35,182 @@ export default function (): JSX.Element {
   }, [navigation])
 
   return (
-    <ScrollView>
-      <Flex padding="medium">
-        <Button
-          onPress={() => {
-            generalContext.toggleDarkMode()
-          }}
-          title={generalContext.darkMode ? 'Light' : 'Dark'}
-        />
-        <Space size="large" />
-        <Text type="h1">Headline 1</Text>
-        <Text type="h2">Headline 2</Text>
-        <Text type="h3">Headline 3</Text>
-        <Text type="h4">Headline 4</Text>
-        <Text type="subtitle1">Subtitle 1</Text>
-        <Text type="subtitle2">Subtitle 2</Text>
-        <Text type="body1">Body 1</Text>
-        <Text type="body2">Body 2</Text>
-        <Text type="body3">Body 3</Text>
-        <Space size="large" />
-        <Input
-          label="Title"
-          left={({ alpha, color }) => (
-            <Icon.ICExclamation {...{ alpha, color }} />
-          )}
-          onChangeText={_text => setText(_text)}
-          placeholder="Enter your title"
-          right={({ alpha, color }) => (
-            <Icon.ICExclamation {...{ alpha, color }} />
-          )}
-          value={text}
-        />
-        <Space size="small" />
-        <Input label="Title" placeholder="Enter your title" />
-        <Space size="small" />
-        <Input
-          label="Title"
-          onChangeText={_text => setText(_text)}
-          placeholder="Enter your title"
-          value={text}
-        />
-        <Space size="small" />
-        <Input editable={false} label="Title" placeholder="Enter your title" />
-        <Space size="small" />
-        <Input
-          label="Title"
-          placeholder="Enter your title"
-          statusMessage="Some tip"
-        />
-        <Space size="small" />
-        <Input
-          label="Title"
-          placeholder="Enter your title"
-          status="error"
-          statusMessage="Your input is error"
-          value={text}
-        />
-        <Space size="small" />
-        <Input
-          label="Title"
-          placeholder="Enter your title"
-          status="success"
-          statusMessage="Your action is Success!"
-          value={text}
-        />
-        <Space size="large" />
-        <Button
-          disabled
-          onPress={() => {
-            //
-          }}
-          title="Button"
-        />
-        <Space size="small" />
-        <Button
-          onPress={() => {
-            //
-          }}
-          title="Button"
-        />
-        <Space size="small" />
-        <Button
-          onPress={() => {
-            //
-          }}
-          size="small"
-          title="Button"
-        />
-        <Space size="small" />
-        <Button
-          disabled
-          onPress={() => {
-            //
-          }}
-          title="Button"
-          type="secondary"
-        />
-        <Space size="small" />
-        <Button
-          onPress={() => {
-            //
-          }}
-          title="Button"
-          type="secondary"
-        />
-        <Space size="small" />
-        <Button
-          onPress={() => {
-            //
-          }}
-          size="small"
-          title="Button"
-          type="secondary"
-        />
-        <Space size="small" />
-        <Button
-          disabled
-          onPress={() => {
-            //
-          }}
-          title="Button"
-          type="tertiary"
-        />
-        <Space size="small" />
-        <Button
-          onPress={() => {
-            //
-          }}
-          title="Button"
-          type="tertiary"
-        />
-        <Space size="small" />
-        <Button
-          onPress={() => {
-            //
-          }}
-          size="small"
-          title="Button"
-          type="tertiary"
-        />
-      </Flex>
-    </ScrollView>
+    <>
+      <ScrollViewFaded
+        colors={{
+          bottom: [
+            hexToRgba(theme.colors.systemBackgroundPrimary, 0),
+            theme.colors.systemBackgroundPrimary
+          ],
+          top: [
+            theme.colors.systemBackgroundPrimary,
+            hexToRgba(theme.colors.systemBackgroundPrimary, 0)
+          ]
+        }}
+        disableTopInset
+      >
+        <Flex paddingHorizontal="medium">
+          <Button
+            onPress={() => {
+              generalContext.toggleDarkMode()
+            }}
+            title={generalContext.darkMode ? 'Light' : 'Dark'}
+          />
+          <Space size="large" />
+          <Text type="h1">Headline 1</Text>
+          <Text type="h2">Headline 2</Text>
+          <Text type="h3">Headline 3</Text>
+          <Text type="h4">Headline 4</Text>
+          <Text type="subtitle1">Subtitle 1</Text>
+          <Text type="subtitle2">Subtitle 2</Text>
+          <Text type="body1">Body 1</Text>
+          <Text type="body2">Body 2</Text>
+          <Text type="body3">Body 3</Text>
+          <Space size="large" />
+          <Input
+            label="Title"
+            left={({ alpha, color }) => (
+              <Icon.ICExclamation {...{ alpha, color }} />
+            )}
+            onChangeText={_text => setText(_text)}
+            placeholder="Enter your title"
+            right={({ alpha, color }) => (
+              <Icon.ICExclamation {...{ alpha, color }} />
+            )}
+            value={text}
+          />
+          <Space size="small" />
+          <Input label="Title" placeholder="Enter your title" />
+          <Space size="small" />
+          <Input
+            label="Title"
+            onChangeText={_text => setText(_text)}
+            placeholder="Enter your title"
+            value={text}
+          />
+          <Space size="small" />
+          <Input
+            editable={false}
+            label="Title"
+            placeholder="Enter your title"
+          />
+          <Space size="small" />
+          <Input
+            label="Title"
+            placeholder="Enter your title"
+            statusMessage="Some tip"
+          />
+          <Space size="small" />
+          <Input
+            label="Title"
+            placeholder="Enter your title"
+            status="error"
+            statusMessage="Your input is error"
+            value={text}
+          />
+          <Space size="small" />
+          <Input
+            label="Title"
+            placeholder="Enter your title"
+            status="success"
+            statusMessage="Your action is Success!"
+            value={text}
+          />
+          <Space size="large" />
+          <Button
+            disabled
+            onPress={() => {
+              //
+            }}
+            title="Button"
+          />
+          <Space size="small" />
+          <Button
+            onPress={() => {
+              //
+            }}
+            title="Button"
+          />
+          <Space size="small" />
+          <Button
+            onPress={() => {
+              //
+            }}
+            size="small"
+            title="Button"
+          />
+          <Space size="small" />
+          <Button
+            disabled
+            onPress={() => {
+              //
+            }}
+            title="Button"
+            type="secondary"
+          />
+          <Space size="small" />
+          <Button
+            onPress={() => {
+              //
+            }}
+            title="Button"
+            type="secondary"
+          />
+          <Space size="small" />
+          <Button
+            onPress={() => {
+              //
+            }}
+            size="small"
+            title="Button"
+            type="secondary"
+          />
+          <Space size="small" />
+          <Button
+            disabled
+            onPress={() => {
+              //
+            }}
+            title="Button"
+            type="tertiary"
+          />
+          <Space size="small" />
+          <Button
+            onPress={() => {
+              //
+            }}
+            title="Button"
+            type="tertiary"
+          />
+          <Space size="small" />
+          <Button
+            onPress={() => {
+              //
+            }}
+            size="small"
+            title="Button"
+            type="tertiary"
+          />
+        </Flex>
+      </ScrollViewFaded>
+      {/* <LinearGradient
+        // colors={[
+        //   hexToRgba(theme.colors.systemBackgroundPrimary, 0),
+        //   theme.colors.systemBackgroundPrimary
+        // ]}
+        colors={[
+          // hexToRgba(theme.colors.systemBackgroundPrimary, 0),
+          // theme.colors.systemBackgroundPrimary
+          'rgb(79, 206, 249)',
+          'rgb(79, 206, 249)'
+        ]}
+        style={{
+          bottom: 0,
+          height: 48 + insets.bottom,
+          position: 'absolute',
+          width: '100%'
+        }}
+      /> */}
+    </>
   )
 }
